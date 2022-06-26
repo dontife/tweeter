58fff52a88ebb13ae9178c1f3287c8bc15ee1acf
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-26 22:27:06 +0000

    added an escape function to prevent cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,48 +5,55 @@
  */
 
 $(document).ready(function() {
+  // Preventing XSS by using am escape function
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
-const renderTweets = function(tweets) {
-  $(".old-tweets").empty();
-  // loops through tweets
-  tweets.map(tweet => {
-    // calls createTweetElement for each tweet
-    let $createTweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $(".old-tweets").prepend($createTweet);
-  })
-}
+  const renderTweets = function(tweets) {
+    $(".old-tweets").empty();
+    // loops through tweets
+    tweets.map(tweet => {
+      // calls createTweetElement for each tweet
+      let $createTweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $(".old-tweets").prepend($createTweet);
+    })
+  }
 
-const createTweetElement = function(tweet) {
-  let date = new Date(tweet.created_at);
-  let $tweet =  $(`
-  <article class = "timeline">
-    <header class='timeline-tweet'>
-       <div class="leftside-info">
-        <img class="mini-avatar" src=${tweet.user.avatars}>
-        <p class="user">${tweet.user.name}</p>
-      </div >
-      <div class="rightside-info">
-        <p class="username">${tweet.user.handle}</p>
-      </div>
-    </header> 
-    <div class="old-tweets-text">${tweet.content.text}</div>
-    <hr class = 'line'>
-    <footer class="footer-info">
-      <output name="date" id="date">${timeago.format(date)}</output>
-      <div id="icons">
-        <span>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </span>
-      </div>
-    </footer>
-  </article> 
-  `);  
-  return $tweet;
-}
+  
+  const createTweetElement = function(tweet) {
+    let date = new Date(tweet.created_at);
+    let $tweet =  $(`
+    <article class = "timeline">
+      <header class='timeline-tweet'>
+         <div class="leftside-info">
+          <img class="mini-avatar" src=${tweet.user.avatars}>
+          <p class="user">${tweet.user.name}</p>
+        </div >
+        <div class="rightside-info">
+          <p class="username">${tweet.user.handle}</p>
+        </div>
+      </header> 
+      <p class="old-tweets-text">${escape(tweet.content.text)}</p>
+      <hr class = 'line'>
+      <footer class="footer-info">
+        <output name="date" id="date">${timeago.format(date)}</output>
+        <div id="icons">
+          <span>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </span>
+        </div>
+      </footer>
+    </article> 
+    `);  
+    return $tweet;
+  }
 
 
   $('form').submit(function(event) {
@@ -88,4 +95,4 @@ const createTweetElement = function(tweet) {
   loadTweets();
 
 
-})
+});
